ec7fff5eb79233c8564873c6ce134219f648116b
paveleroshkinweb <eroshkin321@gmail.com>
2023-05-20 18:11:42 +0300

    update state structure

--- postgres_to_es/src/state/redis_state_storage.py
+++ postgres_to_es/src/state/redis_state_storage.py
@@ -6,15 +6,13 @@ from .state_storage import StateStorage
 
 class RedisStateStorage(StateStorage):
 
-    STATE_KEY = "state"
-
     def __init__(self, redis_client: Redis) -> None:
         self.redis_client = redis_client
 
-    def save_state(self, state: ETLState) -> None:
-        self.redis_client.hmset(self.STATE_KEY, state.dict())
+    def save_state(self, key: str, state: ETLState) -> None:
+        self.redis_client.hmset(key, state.dict())
 
-    def retrieve_state(self) -> ETLState:
-        raw_last_state = self.redis_client.hgetall(self.STATE_KEY)
+    def retrieve_state(self, key: str) -> ETLState:
+        raw_last_state = self.redis_client.hgetall(key)
         if raw_last_state:
             return ETLState(**raw_last_state)
--- postgres_to_es/src/state/state_storage.py
+++ postgres_to_es/src/state/state_storage.py
@@ -6,9 +6,9 @@ from model import ETLState
 class StateStorage(ABC):
 
     @abstractmethod
-    def save_state(self, state: ETLState) -> None:
+    def save_state(self, key: str, state: ETLState) -> None:
         pass
 
     @abstractmethod
-    def retrieve_state(self) -> ETLState:
+    def retrieve_state(self, key: str) -> ETLState:
         pass
